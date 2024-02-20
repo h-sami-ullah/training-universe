e5665774ce7a1613e0bbe513e7bb27e69723c1a4
Hafiz <csamiullah9@gmail.com>
2024-02-20 00:19:14 -0700

    adding main entry point for app

--- api/run.py
+++ api/run.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 import logging
-from api.routes.prediction import router as prediction_router
 import os
 import uvicorn
+from api.app import create_app
 
+app: FastAPI = create_app()
 
-def create_api_desc()->str:
-    desc=''
-    for line in open(r'README.md','r').readlines():
-        desc+=line+'\n'
-    return desc
 
 def setup_logging():
     """Set up logging to file and console."""
@@ -22,17 +18,17 @@ def setup_logging():
                                   logging.StreamHandler()])
     return log_filename
 
-app:FastAPI = FastAPI(title="Restaurant Hotspot Prediction Service",
-                      description=create_api_desc())
+
 def main():
     setup_logging()
+    logging.info("API Starting")
+    PORT = 5005
+    if 'PORT' in os.environ.keys():
+        PORT = int(os.environ['PORT'])
 
-    app.include_router(prediction_router)
-    uvicorn.run(app, host="0.0.0.0", port=5005)
-
+    uvicorn.run(app, host="0.0.0.0", port=PORT, loop="auto", log_level='info')
+    logging.info("API Stopping")
 
 
 if __name__ == "__main__":
     main()
-
-
